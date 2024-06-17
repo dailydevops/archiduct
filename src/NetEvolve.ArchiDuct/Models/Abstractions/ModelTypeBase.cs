d89b8435dc05f6cec6916d7800e1f0cb6fa133d5
Martin Stühmer <me@samtrion.net>
2024-06-17 15:56:34 +0200

    chore: Removed namespaces

--- src/NetEvolve.ArchiDuct/Models/Abstractions/ModelTypeBase.cs
+++ src/NetEvolve.ArchiDuct/Models/Abstractions/ModelTypeBase.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Xml.Linq;
 using ICSharpCode.Decompiler.Documentation;
 using ICSharpCode.Decompiler.TypeSystem;
