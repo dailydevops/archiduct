3e5b9881539f3e3dd30ed781eefbab851bca31ff
Martin Stühmer <me@samtrion.net>
2024-06-18 04:49:39 +0200

    chore: Added `AssemblyReferences`

---
+++ tests/NetEvolve.ArchiDuct.Tests.Integration/Internals/Decompiler_NetEvolveArchiDuct_Tests.cs
@@ -0,0 +1,7 @@
+﻿namespace NetEvolve.ArchiDuct.Tests.Integration.Internals;
+
+using NetEvolve.ArchiDuct.Tests.Integration._internals;
+
+public class Decompiler_NetEvolveArchiDuct_Tests(
+    GenericTypeProvider<ArchitectureCollector> provider
+) : AssembliesTestCaseBase<ArchitectureCollector>(provider) { }
---
+++ tests/NetEvolve.ArchiDuct.Tests.Integration/_internals/AssembliesTestCaseBase.cs
@@ -0,0 +1,20 @@
+﻿namespace NetEvolve.ArchiDuct.Tests.Integration._internals;
+
+using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
+
+public abstract class AssembliesTestCaseBase<TTestCase>(GenericTypeProvider<TTestCase> provider)
+    : TestCaseBase<TTestCase>(provider, true)
+    where TTestCase : notnull
+{
+    [Fact]
+    public async Task Verify_Assemblies()
+    {
+        _ = Parallel.ForEach(
+            _provider.Architecture.Assemblies,
+            assembly => assembly.Value.Types.Clear()
+        );
+        _ = await Verifier.Verify(_provider.Architecture.Assemblies);
+    }
+}
