b80a0b8925357493eee5f001585e5a9fa022d23d
Martin Stühmer <me@samtrion.net>
2024-06-20 23:55:08 +0200

    chore: More logs

--- src/NetEvolve.ArchiDuct/Internals/ModelFactory.cs
+++ src/NetEvolve.ArchiDuct/Internals/ModelFactory.cs
@@ -33,6 +33,7 @@ internal static class ModelFactory
         IDocumentationProvider?
     > _documentationProviders = new(new ModuleEqualityComparer());
 
+    [SuppressMessage("ApiDesign", "RS0030:Do not use banned APIs", Justification = "<Pending>")]
     public static bool TryGetDocumentationProvider(
         IModule module,
         [NotNullWhen(true)] out IDocumentationProvider? documentationProvider
@@ -45,28 +46,20 @@ internal static class ModelFactory
                 XmlDocumentationProvider? provider = null;
                 if (m.PEFile is not null && File.Exists(m.PEFile.FileName))
                 {
-#pragma warning disable RS0030 // Do not use banned APIs
                     Console.Write($"Loading documentation for {m.PEFile.FileName}");
-#pragma warning restore RS0030 // Do not use banned APIs
 
                     provider = DocumentationLoader.LoadDocumentation(m.PEFile);
 
-#pragma warning disable RS0030 // Do not use banned APIs
                     Console.WriteLine(provider is not null ? " - Successful" : " - Failed");
-#pragma warning restore RS0030 // Do not use banned APIs
                 }
 
                 if (provider is null)
                 {
-#pragma warning disable RS0030 // Do not use banned APIs
                     Console.Write("Loading mscorlib documentation");
-#pragma warning restore RS0030 // Do not use banned APIs
 
                     provider = DocumentationLoader.MscorlibDocumentation;
 
-#pragma warning disable RS0030 // Do not use banned APIs
                     Console.WriteLine(provider is not null ? " - Successful" : " - Failed");
-#pragma warning restore RS0030 // Do not use banned APIs
                 }
 
                 return provider;
