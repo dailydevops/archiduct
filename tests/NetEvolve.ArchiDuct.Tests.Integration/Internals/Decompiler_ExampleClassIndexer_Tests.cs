6b74ed9562e0992771806f145d0fccc03a8a8814
Martin Stühmer <me@samtrion.net>
2024-06-24 23:21:58 +0200

    chore: Added Tests for indexer

---
+++ tests/NetEvolve.ArchiDuct.Tests.Integration/Internals/Decompiler_ExampleClassIndexer_Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace NetEvolve.ArchiDuct.Tests.Integration.Internals;
+
+using NetEvolve.ArchiDuct.Tests.Integration._internals;
+
+public class Decompiler_ExampleClassIndexer_Tests(ClassIndexerTypeProvider provider)
+    : TestCaseBase<ClassIndexerTypeProvider>(provider) { }
+
+public sealed class ClassIndexerTypeProvider() : TypeProviderBase(typeof(IndexerClass)) { }
+
+public class IndexerClass : IIndexInterface
+{
+    private int[] arr = new int[100];
+    public int this[int index] // indexer declaration
+    {
+        // The arr object will throw IndexOutOfRange exception.
+        get => arr[index];
+        set => arr[index] = value;
+    }
+}
