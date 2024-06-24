90cd0f722d99bade846c9bcdb09de69018d4931f
Martin Stühmer <me@samtrion.net>
2024-06-24 23:24:22 +0200

    chore: Added Tests for finalizer

---
+++ tests/NetEvolve.ArchiDuct.Tests.Integration/Internals/Decompiler_ExampleFinalizer_Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace NetEvolve.ArchiDuct.Tests.Integration.Internals;
+
+using System;
+using NetEvolve.ArchiDuct.Tests.Integration._internals;
+
+public class Decompiler_ExampleFinalizer_Tests(FinalizerClassTypeProvider provider)
+    : TestCaseBase<FinalizerClassTypeProvider>(provider) { }
+
+public sealed class FinalizerClassTypeProvider() : TypeProviderBase(typeof(FinalizerClass)) { }
+
+public class FinalizerClass
+{
+    public override string ToString() => GetType().Name;
+
+    ~FinalizerClass() => Console.WriteLine($"The {ToString()} finalizer is executing.");
+}
