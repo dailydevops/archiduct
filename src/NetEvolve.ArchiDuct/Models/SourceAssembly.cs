ab8b6f6a7442fe9edc6457c34e2234419ac39df8
Martin Stühmer <me@samtrion.net>
2024-09-09 19:28:13 +0200

    test: Added Tests for `ArchitectureCollector`

--- src/NetEvolve.ArchiDuct/Models/SourceAssembly.cs
+++ src/NetEvolve.ArchiDuct/Models/SourceAssembly.cs
@@ -3,7 +3,16 @@
 using System.Collections.Generic;
 using System.Reflection;
 
-internal sealed record SourceAssembly(Assembly Assembly)
+internal readonly struct SourceAssembly : IEquatable<SourceAssembly>
 {
     public HashSet<SourceFilter> Filters { get; } = [];
+    public Assembly Assembly { get; }
+
+    public SourceAssembly(Assembly assembly) => Assembly = assembly;
+
+    public bool Equals(SourceAssembly other) => Assembly.Equals(other.Assembly);
+
+    public override bool Equals(object? obj) => obj is SourceAssembly other && Equals(other);
+
+    public override int GetHashCode() => Assembly.GetHashCode();
 }
