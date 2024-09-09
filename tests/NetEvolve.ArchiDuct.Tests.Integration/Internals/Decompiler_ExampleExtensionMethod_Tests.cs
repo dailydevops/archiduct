ed6f6b7e97dfc62c932acf2376f08a79a45f8b70
Martin Stühmer <me@samtrion.net>
2024-09-09 20:49:08 +0200

    test: Added Examples for Extension Methods

---
+++ tests/NetEvolve.ArchiDuct.Tests.Integration/Internals/Decompiler_ExampleExtensionMethod_Tests.cs
@@ -0,0 +1,37 @@
+﻿namespace NetEvolve.ArchiDuct.Tests.Integration.Internals;
+
+using System;
+using System.Reflection;
+using NetEvolve.ArchiDuct.Tests.Integration._internals;
+
+public class Decompiler_ExampleExtensionMethod_Tests(ExtensionExampleTypeProvider provider)
+    : TestCaseBase<ExtensionExampleTypeProvider>(provider) { }
+
+public sealed class ExtensionExampleTypeProvider() : TypeProviderBase(typeof(ExtensionExample)) { }
+
+public static class ExtensionExample
+{
+    public static void Deconstruct(
+        this PropertyInfo p,
+        out bool isStatic,
+        out bool isReadOnly,
+        out bool isIndexed,
+        out Type propertyType
+    )
+    {
+        ArgumentNullException.ThrowIfNull(p);
+
+        var getter = p.GetMethod;
+        isReadOnly = !p.CanWrite;
+        isStatic = getter!.IsStatic;
+        isIndexed = p.GetIndexParameters().Length > 0;
+        propertyType = p.PropertyType;
+    }
+
+    public static void Deconstruct<T>(this T? nullable, out bool hasValue, out T value)
+        where T : struct
+    {
+        hasValue = nullable.HasValue;
+        value = nullable.GetValueOrDefault();
+    }
+}
