d68dc6c3b4f2635a95c23a8685080eb8e5da86ac
Martin Stühmer <me@samtrion.net>
2024-09-09 19:27:35 +0200

    test: Added additional tests for `System.Int` and `System.Double`

---
+++ tests/NetEvolve.ArchiDuct.Tests.Integration/Internals/Decompiler_SystemInt_Tests.cs
@@ -0,0 +1,6 @@
+﻿namespace NetEvolve.ArchiDuct.Tests.Integration.Internals;
+
+using NetEvolve.ArchiDuct.Tests.Integration._internals;
+
+public class Decompiler_SystemInt_Tests(GenericTypeProvider<int> provider)
+    : TypesTestCaseGenericBase<int>(provider) { }
