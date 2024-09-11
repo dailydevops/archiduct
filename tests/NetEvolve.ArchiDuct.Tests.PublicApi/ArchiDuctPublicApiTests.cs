74ddd0c135192ae355c14a7d0cc0a5300ecde9bf
Martin Stühmer <me@samtrion.net>
2024-09-11 10:54:16 +0200

    chore: Added Integration Tests for Public API

---
+++ tests/NetEvolve.ArchiDuct.Tests.PublicApi/ArchiDuctPublicApiTests.cs
@@ -0,0 +1,58 @@
+﻿namespace NetEvolve.ArchiDuct.Tests.PublicApi;
+
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+using PublicApiGenerator;
+using VerifyXunit;
+using Xunit;
+
+public class ArchiDuctPublicApiTests
+{
+    [Fact]
+    public Task PublicApi_HasNotChanged_Expected()
+    {
+        var assembly = typeof(ArchitectureCollector).Assembly;
+        var types = assembly.GetTypes().Where(IsVisibleToIntelliSense).ToArray();
+
+        var options = new ApiGeneratorOptions
+        {
+            ExcludeAttributes =
+            [
+                typeof(InternalsVisibleToAttribute).FullName!,
+                "System.Runtime.CompilerServices.IsByRefLikeAttribute",
+                typeof(TargetFrameworkAttribute).FullName!,
+                typeof(CLSCompliantAttribute).FullName!,
+                typeof(AssemblyMetadataAttribute).FullName!,
+                typeof(NeutralResourcesLanguageAttribute).FullName!,
+            ],
+            IncludeTypes = types,
+        };
+
+        var publicApi = assembly.GeneratePublicApi(options);
+
+        return Verifier.Verify(publicApi);
+    }
+
+    private static bool IsVisibleToIntelliSense(Type type)
+    {
+        var browsable = type.GetCustomAttribute<BrowsableAttribute>();
+        if (browsable is null || browsable.Browsable)
+        {
+            return true;
+        }
+
+        var editorBrowsable = type.GetCustomAttribute<EditorBrowsableAttribute>();
+        if (editorBrowsable is null || editorBrowsable.State != EditorBrowsableState.Never)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ tests/NetEvolve.ArchiDuct.Tests.PublicApi/Predefined.cs
@@ -0,0 +1,31 @@
+﻿namespace NetEvolve.ArchiDuct.Tests.PublicApi;
+
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using NetEvolve.ArchiDuct.Models.Documentation;
+using VerifyTests;
+using VerifyXunit;
+
+internal static class Predefined
+{
+    [ModuleInitializer]
+    public static void Init()
+    {
+        Verifier.DerivePathInfo(
+            (sourceFile, projectDirectory, type, method) =>
+            {
+                var directory = Path.Combine(
+                    projectDirectory,
+                    "_snapshots",
+                    Namer.TargetFrameworkNameAndVersion
+                );
+                _ = Directory.CreateDirectory(directory);
+                return new(directory, type.Name, method.Name);
+            }
+        );
+
+        VerifierSettings.AutoVerify(includeBuildServer: false);
+        VerifierSettings.SortPropertiesAlphabetically();
+    }
+}
