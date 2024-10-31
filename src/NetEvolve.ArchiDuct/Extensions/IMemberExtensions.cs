04de71a4d2423462de83cb22960a2d30d2bdba88
Martin Stühmer <me@samtrion.net>
2024-10-31 13:53:49 +0100

    chore: removed duplicated code (#44)

--- src/NetEvolve.ArchiDuct/Extensions/IMemberExtensions.cs
+++ src/NetEvolve.ArchiDuct/Extensions/IMemberExtensions.cs
@@ -1,6 +1,5 @@
 ﻿namespace NetEvolve.ArchiDuct.Extensions;
 
-using System.Reflection.Metadata;
 using ICSharpCode.Decompiler.TypeSystem;
 using ICSharpCode.Decompiler.TypeSystem.Implementation;
 
@@ -8,8 +7,7 @@ internal static class IMemberExtensions
 {
     public static bool IsUnsafe(this IMember member) =>
         IsUnsafe(member.ReturnType)
-        || (member is IParameterizedMember pm && pm.Parameters.Any(p => IsUnsafe(p.Type)))
-        || (member is IMethod m && m.GetBody() is MethodBodyBlock body && body.IsUnsafe());
+        || (member is IParameterizedMember method && method.Parameters.Any(p => IsUnsafe(p.Type)));
 
     private static bool IsUnsafe(IType type) =>
         type.Kind switch
--- src/NetEvolve.ArchiDuct/Extensions/IMethodExtensions.cs
+++ src/NetEvolve.ArchiDuct/Extensions/IMethodExtensions.cs
@@ -4,22 +4,6 @@ using System.Runtime.CompilerServices;
 using ICSharpCode.Decompiler.TypeSystem;
 using ICSharpCode.Decompiler.TypeSystem.Implementation;
 
-internal static class IMemberExtensions
-{
-    public static bool IsUnsafe(this IMember member) =>
-        IsUnsafe(member.ReturnType)
-        || (member is IParameterizedMember method && method.Parameters.Any(p => IsUnsafe(p.Type)));
-
-    private static bool IsUnsafe(IType type) =>
-        type.Kind switch
-        {
-            TypeKind.Pointer or TypeKind.FunctionPointer => true,
-            TypeKind.ByReference or TypeKind.Array when type is TypeWithElementType elementType =>
-                IsUnsafe(elementType.ElementType),
-            _ => false,
-        };
-}
-
 internal static class IMethodExtensions
 {
     public static bool IsExtern(this IMethod method)
