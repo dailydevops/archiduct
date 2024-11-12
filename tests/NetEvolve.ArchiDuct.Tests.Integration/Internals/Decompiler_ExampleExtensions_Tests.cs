9e54b72e23c7dcc9e3cdaa2d73f2c18d2a3e7b2f
Martin Stühmer <me@samtrion.net>
2024-11-12 12:24:35 +0100

    chore: Added additional tests for explicit events and extension methods

---
+++ tests/NetEvolve.ArchiDuct.Tests.Integration/Internals/Decompiler_ExampleExtensions_Tests.cs
@@ -0,0 +1,15 @@
+﻿namespace NetEvolve.ArchiDuct.Tests.Integration.Internals;
+
+using NetEvolve.ArchiDuct.Tests.Integration._internals;
+
+public class Decompiler_ExampleExtensions_Tests(ExtensionsTypeProvider provider)
+    : TestCaseBase<ExtensionsTypeProvider>(provider) { }
+
+public class ExtensionsTypeProvider() : TypeProviderBase(typeof(ExampleExtensions)) { }
+
+internal static class ExampleExtensions
+{
+    public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
+
+    public static bool IsNullOrWhiteSpace(this string value) => string.IsNullOrWhiteSpace(value);
+}
