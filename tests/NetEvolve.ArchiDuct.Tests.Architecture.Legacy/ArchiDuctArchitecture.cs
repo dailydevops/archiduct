c75c13e6848455e131a012cd13ae9bfcdf0e1f34
Martin Stühmer <me@samtrion.net>
2024-11-19 01:18:09 +0100

    style: fixed styling after csharpier update

--- tests/NetEvolve.ArchiDuct.Tests.Architecture.Legacy/ArchiDuctArchitecture.cs
+++ tests/NetEvolve.ArchiDuct.Tests.Architecture.Legacy/ArchiDuctArchitecture.cs
@@ -4,8 +4,10 @@ using System;
 
 internal static class ArchiDuctArchitecture
 {
-    private static readonly Lazy<ArchUnitNET.Domain.Architecture> _instance =
-        new(LoadArchitecture, System.Threading.LazyThreadSafetyMode.PublicationOnly);
+    private static readonly Lazy<ArchUnitNET.Domain.Architecture> _instance = new(
+        LoadArchitecture,
+        System.Threading.LazyThreadSafetyMode.PublicationOnly
+    );
 
     public static ArchUnitNET.Domain.Architecture Instance => _instance.Value;
 
