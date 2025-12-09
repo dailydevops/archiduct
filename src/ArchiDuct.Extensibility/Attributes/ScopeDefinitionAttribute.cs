2ca2b477c153d75a12c3bbc3a1c8bab40589c04a
Martin Stühmer <me@samtrion.net>
2025-12-09 09:01:50 +0100

    feat: Added `ArchiDuct.Extensibilty` Project

---
+++ src/ArchiDuct.Extensibility/Attributes/ScopeDefinitionAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace ArchiDuct.Attributes;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public sealed class ScopeDefinitionAttribute : Attribute
+{
+    public string? DefinitionName { get; }
+
+    public ScopeDefinitionAttribute(string? definitionName = null) => DefinitionName = definitionName;
+}
---
+++ src/ArchiDuct.Extensibility/Attributes/ScopeUsageAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace ArchiDuct.Attributes;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+public sealed class ScopeUsageAttribute : Attribute
+{
+    public string? DefinitionName { get; }
+
+    public ScopeUsageAttribute(string? definitionName = null) => DefinitionName = definitionName;
+}
